86112fbd48101a02cda55e99a9e5e6627b9bec75
Bob The Builder <151051388+RIZKI756@users.noreply.github.com>
2025-05-12 12:42:26 +0200

    Update Favalier.java

--- Favalier.java
+++ Favalier.java
@@ -1,1 +1,37 @@
+import java.util.ArrayList;
+
+class Favalier extends Piece {
+
+    public Favalier() {
+        super('B', new Position());
+    }
+
+    public Favalier(char couleur, Position position) {
+        super(couleur, position);
+    }
+
+    public String getType() {
+        return "favalier";
+    }
+
+    public ArrayList<Position> getDeplacementPossible(Plateau pl) {
+        ArrayList<Position> liste = new ArrayList<>();
+
+        Fou f = new Fou(this.getCouleur(), this.getPosition());
+        liste.addAll(f.getDeplacementPossible(pl));
+
+     
+        Cavalier c = new Cavalier(this.getCouleur(), this.getPosition());
+        ArrayList<Position> deplacementsCavalier = c.getDeplacementPossible(pl);
+
+        
+        for (Position p : deplacementsCavalier) {
+            if (!liste.contains(p)) {
+                liste.add(p);
+            }
+        }
+
+        return liste;
+    }
+}
 
