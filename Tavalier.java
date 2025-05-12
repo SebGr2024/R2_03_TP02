862a51f177cbf60d9f9905ec8e5c30df3b8a0e85
GROUE_sebastien <sebgroue@gmail.com>
2025-05-12 12:36:41 +0200

    Creation Tavalier.java
    Commencement de la class Tavalier

--- Tavalier.java
+++ Tavalier.java
@@ -1,1 +1,24 @@
+import java.util.ArrayList;
+
+class Tavalier extends Piece{
+    
+    public Tavalier(){
+	super('B', new Position());
+    }
+
+    public Tavalier(char couleur, Position position){
+	super(couleur, position);
+    }
+
+
+    public String getType(){
+    	return new String("tavalier");
+    }
+    
+
+    public ArrayList<Position> getDeplacementPossible(Plateau pl)
+    {
+	ArrayList<Position> liste = new ArrayList<Position>();
+	int positionDepartX = this.getPosition().getX();
+	int positionDepartY = this.getPosition().getY();
 
