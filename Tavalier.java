0878b2f9e26c85137ad560684a515a199514034d
GROUE_sebastien <sebgroue@gmail.com>
2025-05-17 16:30:15 +0200

    Update Tavalier.java
    Test de la classe Tavalier avec le code du cavalier et de la tour

--- Tavalier.java
+++ Tavalier.java
@@ -86,3 +86,121 @@ class Tavalier extends Piece{
 	    }
 	    indiceX = indiceX+1;
 
+	int indiceX = positionDepartX-2;
+	int indiceY = positionDepartY-1;
+	if((indiceX >= 0) && (indiceX < 8) && (indiceY >= 0) && (indiceY < 8)){
+	    Piece pi = pl.getCase(indiceX, indiceY);
+	    if(pi == null)
+		liste.add(new Position(indiceX, indiceY));
+	    else if(pi.getCouleur() != this.getCouleur())
+		liste.add(new Position(indiceX, indiceY));
+	}
+
+	// . . . . .
+	// X . . . .
+	// . . C . .
+	// . . . . .
+	// . . . . .
+	indiceX = positionDepartX-2;
+	indiceY = positionDepartY+1;
+	if((indiceX >= 0) && (indiceX < 8) && (indiceY >= 0) && (indiceY < 8)){
+	    Piece pi = pl.getCase(indiceX, indiceY);
+	    if(pi == null)
+		liste.add(new Position(indiceX, indiceY));
+	    else if(pi.getCouleur() != this.getCouleur())
+		liste.add(new Position(indiceX, indiceY));
+	}
+
+	// . X . . .
+	// . . . . .
+	// . . C . .
+	// . . . . .
+	// . . . . .
+	indiceX = positionDepartX-1;
+	indiceY = positionDepartY+2;
+	if((indiceX >= 0) && (indiceX < 8) && (indiceY >= 0) && (indiceY < 8)){
+	    Piece pi = pl.getCase(indiceX, indiceY);
+	    if(pi == null)
+		liste.add(new Position(indiceX, indiceY));
+	    else if(pi.getCouleur() != this.getCouleur())
+		liste.add(new Position(indiceX, indiceY));
+	}
+
+	// . . . X .
+	// . . . . .
+	// . . C . .
+	// . . . . .
+	// . . . . .
+	indiceX = positionDepartX+1;
+	indiceY = positionDepartY+2;
+	if((indiceX >= 0) && (indiceX < 8) && (indiceY >= 0) && (indiceY < 8)){
+	    Piece pi = pl.getCase(indiceX, indiceY);
+	    if(pi == null)
+		liste.add(new Position(indiceX, indiceY));
+	    else if(pi.getCouleur() != this.getCouleur())
+		liste.add(new Position(indiceX, indiceY));
+	}
+
+	// . . . . .
+	// . . . . X
+	// . . C . .
+	// . . . . .
+	// . . . . .
+	indiceX = positionDepartX+2;
+	indiceY = positionDepartY+1;
+	if((indiceX >= 0) && (indiceX < 8) && (indiceY >= 0) && (indiceY < 8)){
+	    Piece pi = pl.getCase(indiceX, indiceY);
+	    if(pi == null)
+		liste.add(new Position(indiceX, indiceY));
+	    else if(pi.getCouleur() != this.getCouleur())
+		liste.add(new Position(indiceX, indiceY));
+	}
+
+	// . . . . .
+	// . . . . .
+	// . . C . .
+	// . . . . X
+	// . . . . .
+	indiceX = positionDepartX+2;
+	indiceY = positionDepartY-1;
+	if((indiceX >= 0) && (indiceX < 8) && (indiceY >= 0) && (indiceY < 8)){
+	    Piece pi = pl.getCase(indiceX, indiceY);
+	    if(pi == null)
+		liste.add(new Position(indiceX, indiceY));
+	    else if(pi.getCouleur() != this.getCouleur())
+		liste.add(new Position(indiceX, indiceY));
+	}
+
+	// . . . . .
+	// . . . . .
+	// . . C . .
+	// . . . . .
+	// . . . X .
+	indiceX = positionDepartX+1;
+	indiceY = positionDepartY-2;
+	if((indiceX >= 0) && (indiceX < 8) && (indiceY >= 0) && (indiceY < 8)){
+	    Piece pi = pl.getCase(indiceX, indiceY);
+	    if(pi == null)
+		liste.add(new Position(indiceX, indiceY));
+	    else if(pi.getCouleur() != this.getCouleur())
+		liste.add(new Position(indiceX, indiceY));
+	}
+
+	// . . . . .
+	// . . . . .
+	// . . C . .
+	// . . . . .
+	// . X . . .
+	indiceX = positionDepartX-1;
+	indiceY = positionDepartY-2;
+	if((indiceX >= 0) && (indiceX < 8) && (indiceY >= 0) && (indiceY < 8)){
+	    Piece pi = pl.getCase(indiceX, indiceY);
+	    if(pi == null)
+		liste.add(new Position(indiceX, indiceY));
+	    else if(pi.getCouleur() != this.getCouleur())
+		liste.add(new Position(indiceX, indiceY));
+	}
+	return liste;
+	}
+}
+
